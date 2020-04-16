fdab3b4ea1e10d187807472656e956dce740fa5a
Vasyl Kitsyla <wkichira@gmail.com>
2020-04-16 16:42:20 +0300

    Update urls.py

--- list/urls.py
+++ list/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
         path('', views.index, name="index"),
         url(r'project/(?P<pk>\d+)$', views.view_project_info, name="project_info"),
-        url(r'^create/$', views.ProjectCreate.as_view(), name='project_create'),
+        url(r'^create/$', views.project_create, name='project_create'),
         url(r'^project/(?P<pk>\d+)/update/$', views.ProjectUpdate.as_view(), name='project_update'),
         url(r'^project/(?P<pk>\d+)/delete/$', views.ProjectDelete.as_view(), name='project_delete'),
         url(r'^task_create/$', views.task_create, name='task_create'),
